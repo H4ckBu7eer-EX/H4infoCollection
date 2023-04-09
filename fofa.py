e62d86e45b59127df0a222070b68597a03e528be
H4CKBUEER_EX <96736106+H4ckBu7eer-EX@users.noreply.github.com>
2023-04-09 16:54:49 +0800

    fofa
    fofa

--- fofa.py
+++ fofa.py
@@ -1,14 +1,13 @@
-def fofa():
+def fofa_api():
+    import os.path
     print("========FOFA========")
     fofa_email = input("输入FOFA email: ") 
     fofa_key = input("输入FOFA API key: ")
     with open('fofa.json', 'w') as f:
-        f.write('{"fofa_email":"' + fofa_email + '","fofa_key":"' + fofa_key + '"}')
+         f.write('{"fofa_email":"' + fofa_email + '","fofa_key":"' + fofa_key + '"}')
     f.close()
     print("fofa.json已保存")
 
+fofa_api()
 
-
-
-fofa()
 

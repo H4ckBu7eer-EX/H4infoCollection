65087d759734db17eb4bbaaf9905fd43d54381ac
Tug0uFenr4r <alang457@163.com>
2023-04-18 19:10:20 +0800

    更新了存活扫描，使用ICMP扫描，发单次包

--- alive_probe.py
+++ alive_probe.py
@@ -1,2 +1,27 @@
+import os
+from datetime import datetime
+
+
 def alive_probe():
-    print("存活探测")
+    ip = input('请输入探测IP:')
+    dt = datetime.today()
+    if ip != "":
+        try:
+            print("注意！使用的是ICMP探测")
+            alive_probe = os.system('ping -c 1 -w 1 {}'.format(ip))
+
+            if alive_probe:
+                print('not alive!!')
+            else:
+                print('target alive!')
+                log = open('info.h4', 'a', encoding='utf-8')
+                log.write('''
+Time:{}\n
+Target:{}-alive'''.format(
+                    ('{}:{}:{}:{}:{}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute))
+                    , ip))
+
+        except Exception as ex:
+            print(ex)
+    else:
+        print('请输入IP!')

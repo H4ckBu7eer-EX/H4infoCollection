682e91deeac5cbaa95a1f7d55a3c9a0686701006
langjunOVO <alang457@163.com>
2023-04-22 09:32:21 +0800

    集成fofaAPI的获取功能，优化C段扫描

--- moudlue/c_scan.py
+++ moudlue/c_scan.py
@@ -1,2 +1,22 @@
-def c_scan():
+import requests
+import fofa
+
+
+def c_ip_get():
+    session = requests.session()
+    fofaAPI_file = open('./fofaAPI.inf', 'r', encoding='utf-8')
+    fofaAPI = fofaAPI_file.readlines()
+    Email = fofaAPI[1].split('=')[1].replace('\n', '')
+    Key = fofaAPI[2].split('=')[1].replace('\n', '')
     print("C段扫描")
+    url = input('请输入目标域名或者IP(例：www.baidu.com):')
+    results = fofa.normal_query(url, Email, Key)
+    ip = results['ip']
+    return ip
+
+
+def c_scan():
+    C_IP = '.'.join(str(c_ip_get()).split('.')[:3]) + '.'
+
+
+c_scan()
--- moudlue/fofa.py
+++ moudlue/fofa.py
@@ -1,2 +1,22 @@
-def fofa():
-    print("FOFA")
+import requests
+import json
+
+
+def normal_query(url,email,key):
+    '''普通搜索'''
+    api_url = "https://fofa.info/api/v1/host/{}?email={}&key={}".format(url, email, key)
+    header = {'Upgrade-Insecure-Requests': '1',
+                      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+                      }
+    session=requests.session()
+    rs=session.get(api_url,headers=header).text
+    results=json.loads(rs)
+    if 'errmsg' in results:
+        if results['errmsg'] == '[40067] ErrInvalidDomain':
+            print('API错误：未知域名！')
+        elif results['errmsg'] == '[-700] 账号无效':
+            print("API错误：账号无效！")
+    else:
+        return results
+
+

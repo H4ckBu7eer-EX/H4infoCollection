fc869564616aff2defd6bbf15181fb84b45d9743
Tug0uFenr4r <alang457@163.com>
2023-04-23 18:42:41 +0800

    集成fofaAPI的获取功能，优化C段扫描

--- moudlue/c_scan.py
+++ moudlue/c_scan.py
@@ -2,24 +2,33 @@ import requests
 import fofa
 import socket
 
-def c_ip_get():
-    session = requests.session()
-    fofaAPI_file = open('./fofaAPI.inf', 'r', encoding='utf-8')
-    fofaAPI = fofaAPI_file.readlines()
-    Email = fofaAPI[1].split('=')[1].replace('\n', '')
-    Key = fofaAPI[2].split('=')[1].replace('\n', '')
-    print("C段扫描")
-    url = input('请输入目标域名或者IP(例：www.baidu.com):')
-    results = fofa.normal_query(url, Email, Key)
+apifile = open('fofaAPI.inf', 'r', encoding='utf-8').read()
+email = apifile.split('Email=')[1].split('Key=')[0].split('\n')[0]
+key = apifile.split('Email=')[1].split('Key=')[1].split('\n')[0]
+def c_ip_get(url):
+    results = fofa.normal_query(url, email, key)
     ip = results['ip']
-    ports=results['port']
-    return [ip,ports]
+    return ip
 
 
-def c_scan():
-    C_IP = '.'.join(str(c_ip_get()[0]).split('.')[:3]) + '.'
-    ports=c_ip_get()[1]
-    print(C_IP)
-    print(ports)
+def c_scan(url):
+    log=open('c-scanlog.log','a',encoding='utf-8')
+    ip=c_ip_get(url)
+    resulte=fofa.hightlevel_query(ip,email,key,'C')
+    C_=resulte['results']
+    for ips in C_:
+        domain=ips[0]
+        ip=ips[1]
+        protocol=''
+        if ips[2] =='80':
+            protocol='http'
+            print(f'[*]Domain:{domain} IP:{ip} WebProtocol:{protocol}\n')
+            log.write(f'[*]Domain:{domain} IP:{ip} WebProtocol:{protocol}\n')
+        elif ips[2] =='443':
+            protocol ='https'
+            print(f'[*]Domain:{domain} IP:{ip} WebProtocol:{protocol}\n')
+            log.write(f'[*]Domain:{domain} IP:{ip} WebProtocol:{protocol}\n')
 
-c_scan()
+
+
+print(c_scan('www.baidu.com'))
--- moudlue/fofa.py
+++ moudlue/fofa.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import base64
 
 def normal_query(url,email,key):
     '''普通搜索'''
@@ -19,4 +19,18 @@ def normal_query(url,email,key):
     else:
         return results
 
+def hightlevel_query(url,email,key,mod):
+    #https://fofa.info/api/v1/search/all?email=your_email&key=your_key&qbase64=dGl0bGU9ImJpbmci
+    #ip="103.74.192.2/24"
+    session=requests.session()
+    if mod == "C":
+        query_word=f'ip="{url}/24"'
+        qbase64=base64.b64encode(query_word.encode('utf-8'))
+        header = {'Upgrade-Insecure-Requests': '1',
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+                  }
+        api_url=f"https://fofa.info/api/v1/search/all?email={email}&key={key}&qbase64={qbase64.decode()}"
+        rs=session.get(api_url,headers=header).text
+        results = json.loads(rs)
+        return results
 

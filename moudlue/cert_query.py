e37e20aeee3d59ea70c07af1bea909859135614e
Tug0uFenr4r <alang457@163.com>
2023-04-23 19:22:33 +0800

    fofaAPI更新高级搜索语法的证书搜索，cert_query.py完成同证书域名信息搜集

--- moudlue/cert_query.py
+++ moudlue/cert_query.py
@@ -1,2 +1,18 @@
-def cert_query():
-    print("证书查询")
+import fofa
+
+
+apifile = open('fofaAPI.inf', 'r', encoding='utf-8').read()
+email = apifile.split('Email=')[1].split('Key=')[0].split('\n')[0]
+key = apifile.split('Email=')[1].split('Key=')[1].split('\n')[0]
+def cert_query(url):
+    log=open('certlog.log','a',encoding='utf-8')
+    print('同证书域名：')
+    results=fofa.hightlevel_query(url,email,key,'cert')
+    web_message_results=results['results']
+    for web in web_message_results:
+        web_domain_ip=web[0]
+        origin_ip=web[1]
+        print(f'[*]域名：{web_domain_ip} IP：{origin_ip} 证书：{url}')
+        log.write(f'[*]域名：{web_domain_ip} IP：{origin_ip} 证书：{url}\n')
+
+cert_query('baidu')
--- moudlue/fofa.py
+++ moudlue/fofa.py
@@ -23,6 +23,7 @@ def hightlevel_query(url,email,key,mod):
     #https://fofa.info/api/v1/search/all?email=your_email&key=your_key&qbase64=dGl0bGU9ImJpbmci
     #ip="103.74.192.2/24"
     session=requests.session()
+    #C段
     if mod == "C":
         query_word=f'ip="{url}/24"'
         qbase64=base64.b64encode(query_word.encode('utf-8'))
@@ -33,4 +34,14 @@ def hightlevel_query(url,email,key,mod):
         rs=session.get(api_url,headers=header).text
         results = json.loads(rs)
         return results
-
+    #证书查询
+    if mod == "cert":
+        query_word=f'cert="{url}"'
+        qbase64 = base64.b64encode(query_word.encode('utf-8'))
+        header = {'Upgrade-Insecure-Requests': '1',
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+                  }
+        api_url = f"https://fofa.info/api/v1/search/all?email={email}&key={key}&qbase64={qbase64.decode()}"
+        rs = session.get(api_url, headers=header).text
+        results = json.loads(rs)
+        return results

cab4f9191ee1a6bfab4a29c7887ddc1675ea167d
Tug0uFenr4r <alang457@163.com>
2023-04-25 18:47:44 +0800

    修复dir_scan的导入模块问题 增加dns查询模块,dns查询包含dns_query和dns_log两个函数，dns_query为查询,dns_log为记录保存

--- moudlue/dns_query.py
+++ moudlue/dns_query.py
@@ -1,2 +1,25 @@
-def dns_query():
-    print("dns查询")
+import dns.resolver
+from colorama import Fore
+import datetime
+
+
+def dns_query(domain, mode):
+    mode_list = ['A', 'MX', 'NS', 'CNAME']
+    if mode in mode_list:
+        answers = dns.resolver.resolve(domain, mode)
+        for data in answers:
+            print(Fore.GREEN + '[*]' + Fore.WHITE + str(data) + ' ' + mode)
+            dns_log('[*]' + str(data) + ' ' + mode + '\n')
+    else:
+        print('你输入了一个不支持的模式！')
+
+
+def dns_log(line):
+    time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
+    file = open('dnslog.log', 'a', encoding='utf-8')
+    log_data = \
+        f'''
+========-----DNS 查询日志-----==========
+Time: {time} 
+                '''
+    file.write(log_data + line)

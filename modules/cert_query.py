b7edae57ef9279994ed8fdcf39d901ab1d9b21e0
Tug0uFenr4r <alang457@163.com>
2023-04-27 19:04:09 +0800

    优化证书查询代码

--- modules/cert_query.py
+++ modules/cert_query.py
@@ -1,15 +1,13 @@
 import fofa
 
 
-def cert_query(url,email,key):
-    log=open('certlog.log','a',encoding='utf-8')
+def cert_query(url, email, key):
+    log = open('certlog.log', 'a', encoding='utf-8')
     print('同证书域名：')
-    results=fofa.hightlevel_query(url,email,key,'cert')
-    web_message_results=results['results']
+    results = fofa.hightlevel_query(url, email, key, 'cert')
+    web_message_results = results['results']
     for web in web_message_results:
-        web_domain_ip=web[0]
-        origin_ip=web[1]
+        web_domain_ip = web[0]
+        origin_ip = web[1]
         print(f'[*]域名：{web_domain_ip} IP：{origin_ip} 证书：{url}')
         log.write(f'[*]域名：{web_domain_ip} IP：{origin_ip} 证书：{url}\n')
-
-cert_query('baidu')

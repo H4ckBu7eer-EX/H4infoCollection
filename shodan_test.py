2ccc2a9dccd67aa5c813fde592f3c71c946266d4
Tug0uFenr4r <alang457@163.com>
2023-04-29 20:40:14 +0800

    更新撒旦

--- shodan_test.py
+++ shodan_test.py
@@ -1,6 +1,14 @@
 from shodan import Shodan
 
-api = Shodan('TcUXrOqOwnqKs6fP5FpzUD8usnyuv3qv')
 
-ipinfo = api.host('8.8.8.8')
-print(ipinfo)
+def shodan_search(apikey,host):
+    key=apikey
+    shodan_api=Shodan(key)
+    for info in shodan_api.host(host).items():
+        print(info)
+
+
+
+#api = Shodan('TcUXrOqOwnqKs6fP5FpzUD8usnyuv3qv')
+
+shodan_search('TcUXrOqOwnqKs6fP5FpzUD8usnyuv3qv','8.8.8.8')

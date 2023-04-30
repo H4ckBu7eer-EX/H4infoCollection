15f9cb0b2f47abec20fc5b7c811a3783f0796309
H4CKBUEER_EX <96736106+H4ckBu7eer-EX@users.noreply.github.com>
2023-04-30 20:49:51 +0800

    Update whois_query.py

--- modules/whois_query.py
+++ modules/whois_query.py
@@ -1,2 +1,13 @@
-def whois_query():
-    print("whois查询")
+import socket
+
+def whois_query(url):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("whois.iana.org", 43))
+    s.send((url + "\r\n").encode())
+    response = b""
+    while True:
+        data = s.recv(4096)
+        response += data
+        if not data:
+            break
+    print(response.decode())
